fc70191e6fa28eeea59123a206528a49bf410113
harshit <harshit@zeddlabz.com>
2023-09-10 16:10:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const mysql = require("mysql");
+
+const surveyRoutes = require("./routes/survey.js");
+const resultsRoutes = require("./routes/results.js");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "./build")));
+
+
+app.use("/survey", surveyRoutes);
+app.use("/results", resultsRoutes);
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
+
+app.listen(8800, () => {
+  console.log("Connected to server!");
+});
+
+// const db = mysql.createConnection({
+//   host: process.env.HOST,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE,
+// });
+
+// app.get("/", (req, res) => {
+//   const q = 'INSERT INTO name(`roll`) VALUES ("Hello")';
+
+//   db.query(q, (err, data) => {
+//     if (err) return res.status(500).json(err);
+//     console.log("Success");
+//     // return res.status(200).json("Sociodemographic Survey has been sent succesfully");
+//   });
+//   res.send(" Hello");
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("db connect successfully");
+//   }
+// });
